ad4b9633029e3893721ff95bccf83b4c32f260ae
SherifCaptainRed <anshtrips07@gmail.com>
2024-12-02 20:25:07 +0530

    Create hello-world.py

---
+++ 01-Beginner/Day-01/hello-world.py
@@ -0,0 +1,17 @@
+# Day 1 of 45 Days Challenge of writing code
+
+"""
+Introduction: 
+This project is the very first step in your 45-day coding challenge. The "Hello, World!" program is a simple yet significant milestone for any programmer. 
+It is often the first program written when learning a new programming language because it provides a basic introduction to the syntax and structure of the language. 
+In this project, I will create a "Hello, World!" program in Python. This will help me get familiar with the development environment and basic syntax of Python.
+"""
+
+"""
+The print() function in Python is used to display messages or output data to the screen. 
+It can handle multiple objects, which are converted to strings before being printed
+"""
+
+print("Hello World")
+print("Python is easy to get started in coding.")
+print("I can easily write logic in Python")
